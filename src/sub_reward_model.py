653b6b6e77e821e05684f73e8845b02bac4bdfe5
aline osmar <alinemosmar98@gmail.com>
2024-11-27 08:41:09 -0300

    fix bertimbau

--- src/sub_reward_model.py
+++ src/sub_reward_model.py
@@ -1,26 +1,38 @@
-from transformers import AutoConfig, AutoModelForSequenceClassification, BertModel, BertForSequenceClassification
+from transformers import AutoConfig, AutoModel
 import torch.nn as nn
 
 class SubRewardModel(nn.Module):
-
     def __init__(self, dropout=0.2):
         super(SubRewardModel, self).__init__()
-        config = BertModel.from_pretrained('neuralmind/bert-base-portuguese-cased')
+        # Configuração do modelo BERTimbau
+        config = AutoConfig.from_pretrained('neuralmind/bert-base-portuguese-cased')
         config.hidden_dropout_prob = dropout
         config.attention_probs_dropout_prob = dropout
-        self.bert = BertForSequenceClassification.from_pretrained('neuralmind/bert-base-portuguese-cased', config=config)
+
+        # Carregar o modelo base BERTimbau
+        self.bert = AutoModel.from_pretrained('neuralmind/bert-base-portuguese-cased', config=config)
         self.dropout = nn.Dropout(dropout)
+
+        # Camada de regressão linear
         self.regressor = nn.Sequential(
-            nn.Dropout(dropout),
-            nn.Linear(config.hidden_size, 1)  # Utilize config.hidden_size
+            nn.Linear(config.hidden_size, 1)  # Saída de um único valor para regressão
         )
+
+        # Função de perda para tarefas de regressão
         self.loss_fct = nn.MSELoss()
 
-    def forward(self, input_ids, attention_mask, token_type_ids, labels=None):
+    def forward(self, input_ids, attention_mask, token_type_ids=None, labels=None):
+        # Passar os dados pelo modelo BERT
         outputs = self.bert(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
-        pooled_output = outputs.pooler_output  # Utilize o pooler_output para representação [CLS]
+
+        # Obter a representação do token [CLS]
+        pooled_output = outputs.last_hidden_state[:, 0, :]  # Pegando o token [CLS]
         pooled_output = self.dropout(pooled_output)
+
+        # Regressor linear para prever a simplicidade
         logits = self.regressor(pooled_output).clamp(-1, 1)
+
+        # Cálculo da perda, se os rótulos forem fornecidos
         if labels is not None:
             loss = self.loss_fct(logits.view(-1), labels.view(-1))
             return loss, logits
