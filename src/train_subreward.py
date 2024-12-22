e25f4baf5ad2bf29a0245195a6d9d0ff5ea60624
aline osmar <alinemosmar98@gmail.com>
2024-12-22 00:00:39 -0300

    fix

--- src/train_subreward.py
+++ src/train_subreward.py
@@ -58,8 +58,8 @@ def main_worker(gpu, args):
 
         time2 = time.time()
         print('Training epoch {}, total time {:.2f}, loss {:.7f}'.format(epoch, (time2 - time1), train_loss))
-        no_scaled_preds = scaler.inverse_transform(val_preds).flatten()
-        no_scaled_actuals = scaler.inverse_transform(val_actuals).flatten()
+        no_scaled_preds = scaler.inverse_transform(preds).flatten()
+        no_scaled_actuals = scaler.inverse_transform(actuals).flatten()
 
         df = pd.DataFrame({
             'sentence_from': sentence_from,
